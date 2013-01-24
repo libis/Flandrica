6b68aca0c2b8c0dd4958722a8705556b2191fea4
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2013-01-24 10:28:07 +0100

    Put atom fix into seperate plugin (AtomExtend)

---
+++ plugins/AtomExtend/plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+// Add plugin hooks.
+add_plugin_hook('install', 'atomextend_install');
+add_plugin_hook('uninstall', 'atomextend_uninstall');
+
+
+function atomextend_install()
+{
+
+}
+
+function atomextend_uninstall()
+{
+
+}
+
+?>
