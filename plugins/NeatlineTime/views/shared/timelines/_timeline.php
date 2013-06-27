691b1e8050446fe0d566635fba3d2a3e6b4b6a44
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2013-06-27 10:14:54 +0200

    testing #4

--- plugins/NeatlineTime/views/shared/timelines/_timeline.php
+++ plugins/NeatlineTime/views/shared/timelines/_timeline.php
@@ -7,11 +7,10 @@
 <!-- Container. -->
 <div id="<?php echo neatlinetime_timeline_id(); ?>" class="timeline-default" style="height: 600px; margin-top: 20px; margin-bottom: 20px;"></div>
 <script>
-
-    $(document).ready(function() {
+    jQuery(document).ready(function() {
         NeatlineTime.loadTimeline(
             '<?php echo neatlinetime_timeline_id(); ?>',
-            '<?php echo neatlinetime_json_uri_for_timeline()."&".time(); ?>'
+            '<?php echo neatlinetime_json_uri_for_timeline()."&".time().'/'; ?>'
         );
     });
 </script>
