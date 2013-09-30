b01ea17f53bb33870134bcf14fe176b46384860c
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2013-09-30 14:33:59 +0200

    #15 aanpassing apc

--- plugins/Apc/controllers/IndexController.php
+++ plugins/Apc/controllers/IndexController.php
@@ -1,14 +1,13 @@
 <?php
 class Apc_IndexController extends Omeka_Controller_Action
 {
-	public function indexAction(){
-            if (in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
-                    apc_clear_cache();
-                    apc_clear_cache('user');
-                    apc_clear_cache('opcode');
-                    echo json_encode(array('APC Clear Cache' => true));
-            }
-            exit;
-        }
+    public function indexAction(){
+      
+       apc_clear_cache();
+       apc_clear_cache('user');
+       apc_clear_cache('opcode');
+       echo json_encode(array('APC Clear Cache' => true));
 
+       exit;
+    }
 }
