15ed6bf946f5867ef704e83ba22e34eb370f6694
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2013-10-07 16:26:28 +0200

    apc

--- plugins/Apc/controllers/IndexController.php
+++ plugins/Apc/controllers/IndexController.php
@@ -2,7 +2,7 @@
 class Apc_IndexController extends Omeka_Controller_Action
 {
     public function indexAction(){
-      
+       
        apc_clear_cache();
        apc_clear_cache('user');
        apc_clear_cache('opcode');
