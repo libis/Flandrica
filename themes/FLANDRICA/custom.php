15e0b093ce1ac64b3be8d857ec36d53d59231b7e
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2014-08-11 11:11:55 +0200

    fix #29

--- themes/FLANDRICA/custom.php
+++ themes/FLANDRICA/custom.php
@@ -431,9 +431,9 @@ function Libis_link_to_related_exhibits_home($item) {
             WHERE isp.item_id = ?";
         
         if(libis_get_language()=='en'):        
-            $select .= " AND locate('en/',e.slug)>0";
+            $select .= " AND locate('en-',e.slug)>0";
         else:
-            $select .= " AND locate('en/',e.slug)=0";
+            $select .= " AND locate('en-',e.slug)=0";
         endif;
         
 	$exhibits = $db->getTable("Exhibit")->fetchObjects($select,$item->id);
